a56e369da17622d2634c6aef04523a5c4ba4bb95
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-07-01 11:15:36 +0200

    improved phone number guessing

--- SF.Zentrale.LaunchyPlugin/Telephone/TelephoneSystemController.cs
+++ SF.Zentrale.LaunchyPlugin/Telephone/TelephoneSystemController.cs
@@ -9,13 +9,16 @@ namespace SF.Zentrale.LaunchyPlugin.Telephone
 
         public static bool CheckForTelephoneNumber(string userInput)
         {
-            return userInput.StartsWith("TEL") || userInput == "CALL" || IsNumericWithOptionalLeadingPlus(userInput);
+            return userInput.StartsWith("TEL") || userInput == "CALL" || LooksLikeTelephoneNumber(userInput);
         }
 
-        public static bool IsNumericWithOptionalLeadingPlus(string userInput)
+        public static bool LooksLikeTelephoneNumber(string userInput)
         {
             uint exceptCount;
-            return userInput.StripAllNonDigitsExcept('+', out exceptCount).Length.IsBetweenInclusive(2, 15) &&
+            var stripped = userInput.StripAllNonDigitsExcept('+', out exceptCount);
+            var strippedLength = stripped.Length;
+            return (strippedLength.IsBetweenInclusive(4, 15) && ("+0".IndexOf(stripped[0])) >= 0 || // external phone number
+                    (strippedLength.IsBetweenInclusive(2, 3) && char.IsDigit(stripped[0]))) && // internal extension number
                    (exceptCount == 0 || (exceptCount == 1 && userInput[0] == '+'));
         }
 
