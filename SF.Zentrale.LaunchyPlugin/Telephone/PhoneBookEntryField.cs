305347cce8b1c8c0b8ffa7a1a985ad5a36e4e8ec
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-07-09 13:26:41 +0200

    added Kurzwahl flags
    also cleaned up that section

--- SF.Zentrale.LaunchyPlugin/Telephone/PhoneBookEntryField.cs
+++ SF.Zentrale.LaunchyPlugin/Telephone/PhoneBookEntryField.cs
@@ -5,23 +5,33 @@ namespace SF.Zentrale.LaunchyPlugin.Telephone
     [Flags]
     public enum PhoneBookEntryField
     {
-        None = 0,
-        IsMobileFlag = (1 << 17),
-        IsPhoneNumberFlag = (1 << 18),
-        IsSingleValuedNameFieldFlag = (1 << 19),
-        IsOfficeFlag = (1 << 20),
-        IsPrivateFlag = (1 << 21),
+        NamePartOffset = 0,
+        PhoneNumberOffset = 10,
+        FlagOffset = 17,
         
-        Nickname = IsSingleValuedNameFieldFlag | (1),
-        Surname = IsSingleValuedNameFieldFlag | (1 << 1),
-        GivenName = IsSingleValuedNameFieldFlag | (1 << 2),
-        DisplayName = IsSingleValuedNameFieldFlag | (1 << 3),
-        Title = IsSingleValuedNameFieldFlag | (1 << 4),
+        None = 0,
+        IsMobileFlag = 1 << FlagOffset,
+        IsPhoneNumberFlag = 1 << FlagOffset + 1,
+        IsSingleValuedNameFieldFlag = 1 << FlagOffset + 2,
+        IsOfficeFlag = 1 << FlagOffset + 3,
+        IsPrivateFlag = 1 << FlagOffset + 4,
+        IsKurzwahlFlag = 1 << FlagOffset + 5,
 
-        PrivateMobilePhone = IsPrivateFlag | IsPhoneNumberFlag | IsMobileFlag | (1 << 10),
-        PrivatePhoneNumber = IsPrivateFlag | IsPhoneNumberFlag | (1 << 11),
-        BusinessPhoneNumber = IsOfficeFlag | IsPhoneNumberFlag | (1 << 12),
-        BusinessMobilePhone = IsOfficeFlag | IsPhoneNumberFlag | (1 << 13),
+        Nickname = IsSingleValuedNameFieldFlag | 1 << NamePartOffset,
+        Surname = IsSingleValuedNameFieldFlag | 1 << NamePartOffset + 1,
+        GivenName = IsSingleValuedNameFieldFlag | 1 << NamePartOffset + 2,
+        DisplayName = IsSingleValuedNameFieldFlag | 1 << NamePartOffset + 3,
+        Title = IsSingleValuedNameFieldFlag | 1 << NamePartOffset + 4,
+
+        PrivateMobilePhone = IsPrivateFlag | IsPhoneNumberFlag | IsMobileFlag | 1 << PhoneNumberOffset,
+        PrivatePhoneNumber = IsPrivateFlag | IsPhoneNumberFlag | 1 << PhoneNumberOffset + 1,
+        BusinessPhoneNumber = IsOfficeFlag | IsPhoneNumberFlag | (1 << PhoneNumberOffset + 2),
+        BusinessMobilePhone = IsOfficeFlag | IsPhoneNumberFlag | (1 << PhoneNumberOffset + 3),
+
+        PrivateMobilePhoneKurzwahl = IsKurzwahlFlag | PrivateMobilePhone,
+        PrivatePhoneNumberKurzwahl = IsKurzwahlFlag | PrivatePhoneNumber,
+        BusinessPhoneNumberKurzwahl = IsKurzwahlFlag | BusinessPhoneNumber,
+        BusinessMobilePhoneKurzwahl = IsKurzwahlFlag | BusinessMobilePhone
     }
 
     public static class PhoneBookEntryFieldEx
