b7f86e60b021d5d1af62abeeb6cd715a08ea1d25
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-08-30 20:51:40 +0200

    added sfget.cmd

---
+++ homebin/sfget.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+setlocal
+set file=%~1
+set usetype=0
+if [%file%]==[] (
+	mkdir B:\Temp
+	pushd B:\Temp
+	set file=listing.txt
+	set usetype=1
+) else (
+	pushd "%userprofile%\My Videos"
+)
+
+wget -N "http://sfip1.no-ip.org:27081/%file%"
+
+if %usetype% equ 1 (
+	type %file%
+)
+
+popd
+
+endlocal
