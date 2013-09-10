68ff84d77f15090e11f894114650aed0e5a4fc5e
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-09-10 16:12:18 +0200

    added FixedFont struct

--- homebin/src/UnicodeConsole.cs
+++ homebin/src/UnicodeConsole.cs
@@ -1,11 +1,46 @@
-﻿using System.Text;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Microsoft.Win32;
 using System;
 
 namespace UnicodeConsole
 {
+    public class FixedFontList : IEnumerable<FixedFont>
+    {
+        private readonly Dictionary<int, FixedFont> _linkedFonts;
 
+        public FixedFontList(params string[] fonts)
+        {
+            _linkedFonts = new Dictionary<int, FixedFont>(fonts.Length);
+        }
 
+        public void Add(FixedFont font)
+        {
+            if (Contains(font))
+                return;
+
+            var index = _linkedFonts.Count;
+            _linkedFonts[index] = font;
+        }
+
+        private bool Contains(FixedFont fontToFind)
+        {
+            return _linkedFonts.Any(linkedFont => linkedFont.Value == fontToFind);
+        }
+
+        public IEnumerator<FixedFont> GetEnumerator()
+        {
+            return _linkedFonts.Select(font => font.Value).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    
     public class Example
     {
         public static void Main()
@@ -13,12 +48,6 @@ namespace UnicodeConsole
             var unicodeEncoding = new UnicodeEncoding(!BitConverter.IsLittleEndian, false);
             Console.InputEncoding = unicodeEncoding;
             Console.OutputEncoding = unicodeEncoding;
-
-            string valueName = "Consolas";
-            string newFont = "simsun.ttc,SimSun";
-            string[] fonts = null;
-            RegistryValueKind kind = 0;
-            bool toAdd;
 
             RegistryKey fontLinkKey;
             try
@@ -44,8 +73,17 @@ namespace UnicodeConsole
             }
             else
             {
+                var baseFonts = new FixedFontList { new FixedFont("consola.ttf", "Consolas") };
+
+                string valueName = "Consolas";
+                string newFont = "simsun.ttc,SimSun";
+
+                FixedFontList existingLinkedFonts;
+                RegistryValueKind kind = 0;
+                bool toAdd;
+
                 // Determine if the font is a base font. 
-                string[] names = fontLinkKey.GetValueNames();
+                var names = fontLinkKey.GetValueNames();
                 if (Array.Exists(names, s => s.Equals(valueName,
                                              StringComparison.OrdinalIgnoreCase)))
                 {
@@ -56,20 +94,19 @@ namespace UnicodeConsole
                     switch (kind)
                     {
                         case RegistryValueKind.String:
-                            fonts = new[] { (string)fontLinkKey.GetValue(valueName) };
+                            existingLinkedFonts = new FixedFontList((string) fontLinkKey.GetValue(valueName));
                             break;
                         case RegistryValueKind.MultiString:
-                            fonts = (string[])fontLinkKey.GetValue(valueName);
+                            existingLinkedFonts = new FixedFontList((string[])fontLinkKey.GetValue(valueName));
                             break;
                         
                         default:
                             // Do nothing.
-                            fonts = new string[] { };
+                            existingLinkedFonts = new FixedFontList();
                             break;
                     }
-                    // Determine whether SimSun is a linked font. 
-                    if (Array.FindIndex(fonts, s => s.IndexOf("SimSun",
-                                               StringComparison.OrdinalIgnoreCase) >= 0) >= 0)
+                    
+                    if (existingLinkedFonts.Contains())
                     {
                         Console.WriteLine("Font is already linked.");
                         toAdd = false;
@@ -84,18 +121,17 @@ namespace UnicodeConsole
                 {
                     // Font is not a base font.
                     toAdd = true;
-                    fonts = new string[] { };
+                    existingLinkedFonts = new FixedFontList();
                 }
 
                 if (toAdd)
                 {
-                    Array.Resize(ref fonts, fonts.Length + 1);
-                    fonts[fonts.GetUpperBound(0)] = newFont;
+                    existingLinkedFonts.Add();
                     // Change REG_SZ to REG_MULTI_SZ. 
                     if (kind == RegistryValueKind.String)
                         fontLinkKey.DeleteValue(valueName, false);
 
-                    fontLinkKey.SetValue(valueName, fonts, RegistryValueKind.MultiString);
+                    fontLinkKey.SetValue(valueName, existingLinkedFonts.ToRegArray(), RegistryValueKind.MultiString);
                     Console.WriteLine("SimSun added to the list of linked fonts.");
                 }
             }
