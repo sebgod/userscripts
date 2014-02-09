be86c617cdbc6e709fe2733e208f43faa537ccb7
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-02-09 08:54:13 +0100

    other files converted to eol=lf

--- homecfg/.bashrc.d/mercury.sh
+++ homecfg/.bashrc.d/mercury.sh
@@ -1,1 +1,4 @@
 [ -d $MERCURY_HOME/bin ] && export PATH=$MERCURY_HOME/bin:$PATH
+alias e="cd ~/github/mercury/extras"
+alias m="mmc --use-grade-subdirs --make"
+alias sm="sudo mmc --use-grade-subdirs --make"
