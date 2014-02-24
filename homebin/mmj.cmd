9918847bac1c8d34c15a79aaa9254b523744b5be
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-02-24 21:48:31 +0100

    added go+x commands
    appending mercury libraries to the classpath

--- homebin/mmj.cmd
+++ homebin/mmj.cmd
@@ -1,1 +1,14 @@
-@call mmc --use-grade-subdirs --no-detect-libgrades -s java -m %*
+@call userenv
+
+@setlocal enabledelayedexpansion
+
+@set jcp=
+@for %%J in (%mercury_config_dir%\lib\java\*.jar) do @(
+    if [!jcp!]==[] (
+        set jcp=%%J
+    ) else (
+        set jcp=!jcp!;%%J
+    )
+)
+@call %mercury_compiler% --use-grade-subdirs --no-detect-libgrades -s java --java-classpath %jcp% -m %*
+@endlocal
