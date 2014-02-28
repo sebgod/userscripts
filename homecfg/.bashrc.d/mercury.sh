c589f38f8281fac30be606844c5a0cf37749b964
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-02-28 12:19:00 +0100

    Added mdev navigation command

--- homecfg/.bashrc.d/mercury.sh
+++ homecfg/.bashrc.d/mercury.sh
@@ -1,4 +1,5 @@
 [ -z $MERCURY_HOME ] || ( [ -d $MERCURY_HOME/bin ] && export PATH=$MERCURY_HOME/bin:$PATH )
 alias e="cd ~/github/sebgod/mercury/extras"
+alias mdev="cd ~/github/mercury/mercury-dev"
 alias m="mmc --use-grade-subdirs --make"
 alias sm="sudo mmc --use-grade-subdirs --make"
