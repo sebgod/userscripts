f47bab3050dbacc95ac780d9cb2d676b4e178794
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-03-14 23:38:21 +0100

    Now supporting recursive mtag

--- homecfg/bashrc.d/mercury.sh
+++ homecfg/bashrc.d/mercury.sh
@@ -1,5 +1,8 @@
 [ -z $MERCURY_HOME ] || ( [ -d $MERCURY_HOME/bin ] && export PATH=$MERCURY_HOME/bin:$PATH )
-alias e="cd ~/github/sebgod/mercury/extras"
-alias mdev="cd ~/github/mercury/mercury-dev"
+MERCURY_FORK=$HOME/github/sebgod/mercury
+MERCURY_MAIN=$HOME/github/mercury/mercury-dev
+alias e="cd ${MERCURY_FORK}/extras"
+alias mtu="pushd ${MERCURY_FORK}/extras && find .. -name *.m | xargs -- mtags && popd"
+alias mdev="cd $MERCURY_MAIN"
 alias m="mmc --use-grade-subdirs --make"
 alias sm="sudo mmc --use-grade-subdirs --make"
