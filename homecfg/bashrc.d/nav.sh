68181a9306141463c4ad63e63603505a7e7a3034
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-03-19 11:40:11 +0100

    nav: added easy vimfiles navigation

--- homecfg/bashrc.d/nav.sh
+++ homecfg/bashrc.d/nav.sh
@@ -2,6 +2,7 @@
 
 alias gom="cd /media/sf_share/mercury"
 alias goll="cd /media/sf_share/llvm"
+alias govim="cd ~/github/sebgod/userscripts/homecfg/vim"
 alias gogit="cd ~/github/"
 alias gos="cd /media/sf_share"
 alias sego="cd ~/github/sebgod/userscripts/homecfg"
