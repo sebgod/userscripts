1c2520e9784cc82797f9dcda9aa9b1da4d715f23
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-04-03 17:34:54 +0200

    added ucd alias for mercury-unicode

--- homecfg/bashrc.d/mercury.sh
+++ homecfg/bashrc.d/mercury.sh
@@ -1,8 +1,10 @@
 [ -z $MERCURY_HOME ] || ( [ -d $MERCURY_HOME/bin ] && export PATH=$MERCURY_HOME/bin:$PATH )
-MERCURY_FORK=$HOME/github/sebgod/mercury
+SEGO_GITHUB=$HOME/github/sebgod
+MERCURY_FORK=$SEGO_GITHUB/mercury
 MERCURY_MAIN=$HOME/github/mercury/mercury-dev
 alias e="cd ${MERCURY_FORK}/extras"
 alias mtu="pushd ${MERCURY_FORK}/extras && find .. -name *.m | xargs -- mtags && popd"
 alias mdev="cd $MERCURY_MAIN"
+alias ucd="cd ${SEGO_GITHUB}/mercury-unicode"
 alias m="mmc --use-grade-subdirs --make"
 alias sm="sudo mmc --use-grade-subdirs --make"
