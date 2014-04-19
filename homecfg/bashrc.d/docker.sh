39c68fa11ee53a7ead4ea9eb335138eaab569ab7
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-04-19 23:27:26 +0200

    add more docker aliases

--- homecfg/bashrc.d/docker.sh
+++ homecfg/bashrc.d/docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+alias docker-img="sudo docker images"
+alias docker-rmi="sudo docker rmi"
 alias docker-info="sudo docker info"
 alias docker-cleanup="sudo docker rm \`sudo docker ps --no-trunc -a -q\`"
 
