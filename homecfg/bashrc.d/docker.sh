7bad01ac504d04f718781adce8af67e2c637935f
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-04-20 21:44:02 +0200

    added docker: alias -ps and -attach

--- homecfg/bashrc.d/docker.sh
+++ homecfg/bashrc.d/docker.sh
@@ -4,4 +4,5 @@ alias docker-img="sudo docker images"
 alias docker-rmi="sudo docker rmi"
 alias docker-info="sudo docker info"
 alias docker-cleanup="sudo docker rm \`sudo docker ps --no-trunc -a -q\`"
-
+alias docker-attach="sudo docker attach -sig-proxy=false"
+alias docker-ps="sudo docker ps"
