2e29de4820b1ec076356177a0ee092e2ce5eb5d8
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-04-26 20:45:21 +0200

    add build mercury script

---
+++ homecfg/bin/build_mercury.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# INSTALL_GIT - installation instructions and installation script.
+#
+# This version is for use by Mercury developers after you have just
+# checked the files out from the Git archive. 
+# Note: you need a previous version of Mercury already installed, since
+# the Git archive doesn't have all the necessary files for bootstrapping.
+#
+# You also need autoconf (version 2.58 or later) (and hence GNU m4) installed.
+#
+# Step 0.  aclocal -I m4
+#
+# Step 1.  autoconf
+#
+# Step 2.  ./configure
+#
+# Step 3.  mmake depend
+#
+# Step 4.  mmake
+#
+# Step 5.  tools/bootcheck
+#
+# Step 6.  cd stage2 
+#
+# Step 7.  mmake install
+#
+#	   If you just want to do a `make' rather than a `make install',
+#	   then comment out the `mmake install' line below.
+#
+#	   If you want to install without doing a boot check, then comment
+#	   out the `tools/bootcheck' and `cd stage2' lines below.  If the
+#	   version of the compiler you checked out is buggy and you don't
+#	   perform a boot check first, you risk installing a version of the
+#	   compiler incapable of compiling itself.  Make sure you have a
+#	   fall-back plan if you do this.
+#	   Note also that occasionally changes made to the compiler mean
+#	   that installing from a stage 1 build will fail, even when the
+#	   compiler is fine.  In such cases the only way to install the
+#	   compiler is from the stage 2 directory.
+#
+#	   If you don't want to do a parallel make, comment out the
+#	   `parallel=-j3' line below.
+
+pushd ~/github/mercury/mercury-dev
+parallel=-j8
+
+sudo mmake realclean
+aclocal -I m4 &&
+autoconf &&
+./configure --enable-java-grade --enable-csharp-grade --enable-erlang-grade &&
+touch Mmake.params &&
+touch Mercury.options &&
+mmake depend &&
+mmake MMAKEFLAGS=$parallel &&
+#tools/bootcheck $parallel &&
+#cd stage2 &&
+mmake tags
+sudo mmake install MMAKEFLAGS=$parallel &&
+true
+
+popd
