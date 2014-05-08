319a7382bb7169ce388513cbc9b4d21851632dc8
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-05-08 22:03:40 +0200

    add /usr/share/java\* to classpath

--- homecfg/bashrc.d/java.sh
+++ homecfg/bashrc.d/java.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+export CLASSPATH=/usr/share/java/\*:$CLASSPATH
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:bin/javac::")
 
