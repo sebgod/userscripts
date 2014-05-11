76b9a63bcc5fee953e08e88cf725e02f2e7742c8
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-05-11 08:12:21 +0200

    vim:mercuryplugin: add copyright notice; move vimline on top

--- homecfg/vim/ftplugin/mercuryhdr.sh
+++ homecfg/vim/ftplugin/mercuryhdr.sh
@@ -3,10 +3,10 @@
 cat <<EOF
 %------------------------------------------------------------------------------%
 % File: $1
-% Main author: $GIT_AUTHOR_NAME <$GIT_AUTHOR_EMAIL>
-% Copyright (C)
-% Created on: $(date)
 % vim: ft=mercury ff=unix ts=4 sw=4 et
+% Copyright (C) 2014 $GIT_AUTHOR_NAME
+% Main author: $GIT_AUTHOR_NAME <$GIT_AUTHOR_EMAIL>
+% Created on: $(date)
 %
 %------------------------------------------------------------------------------%
 
