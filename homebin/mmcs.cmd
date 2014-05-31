9f47b05188b9db467a089f8851294afc76e7691f
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-05-31 23:50:26 +0200

    mmcs.cmd: streamlined

--- homebin/mmcs.cmd
+++ homebin/mmcs.cmd
@@ -1,5 +1,4 @@
+@setlocal enabledelayedexpansion
 @call %~dp0userenv
-@setlocal
 @set keyfile="%userprofile%\Documents\GitHub\mercury\mercury.snk"
 @call mercury --use-grade-subdirs --no-detect-libgrades --sign-assembly %keyfile% -s csharp -m %*
-@endlocal
