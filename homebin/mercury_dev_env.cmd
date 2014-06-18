9968f0e445bbd05c9822aa5bc4dd79cb04a736eb
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-06-18 16:11:02 +0200

    mercury_dev_env.cmd: fix checking for 'old' SDK include dir

--- homebin/mercury_dev_env.cmd
+++ homebin/mercury_dev_env.cmd
@@ -1,7 +1,26 @@
 @call %~dp0userenv
 
-if exist "%ProgramFiles32%\Microsoft SDKs\Windows\v7.1a\Include" (
-    set INCLUDE="%INCLUDE%;%ProgramFiles32%\Windows\v7.1a\Include"
-)
+@set INCLUDE_ORIG=%INCLUDE%
+@call :FIND_IN_INCLUDE Win32.Mak
+@if errorlevel 1 ( exit /b 0 )
 
-@exit /b 0
+@set INCLUDE=%INCLUDE_ORIG%%ProgramFiles32%\Microsoft SDKs\Windows\v7.0\Include
+@call :FIND_IN_INCLUDE Win32.Mak
+@if errorlevel 1 ( exit /b 0 )
+
+@set INCLUDE=%INCLUDE_ORIG%%ProgramFiles32%\Microsoft SDKs\Windows\v7.1\Include
+@call :FIND_IN_INCLUDE Win32.Mak
+@if errorlevel 1 ( exit /b 0 )
+
+@set INCLUDE=%INCLUDE_ORIG%%ProgramFiles32%\Microsoft SDKs\Windows\v7.1a\Include
+@call :FIND_IN_INCLUDE Win32.Mak
+@if errorlevel 1 ( exit /b 0 )
+
+@exit /b 1
+
+:FIND_IN_INCLUDE
+    @if /I "%~dp$INCLUDE:1" EQU "" (
+        exit /b 0
+    ) else (
+        exit /b 1
+    )
