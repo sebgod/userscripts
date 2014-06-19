27e170b55da78ef600257c8e9192660f60a9b6a8
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-06-20 01:09:08 +0200

    add mmcmake, a way to invoke make with MMC=/correct/path/to/mmc

---
+++ homebin/mmcmake.cmd
@@ -0,0 +1,23 @@
+@setlocal enabledelayedexpansion
+@if defined MERCURY_HOME (
+   call :SET_HOME MMC
+) else (
+   call :FIND_IN_PATH mercury.bat MMC
+)
+
+@if defined MMC goto :MAKE
+@echo Cannot find Mercury compiler executable, MERCURY_HOME=%MERCURY_HOME%
+@exit /b 1
+
+:MAKE
+@make MMC=%MMC% %*
+@set MAKE_RESULT=%ERRORLEVEL%
+@echo result=%MAKE_RESULT%
+@exit /b %MAKE_RESULT%
+
+:SET_HOME
+    @endlocal && ( set %1="%MERCURY_HOME%\bin\mmc" ) && exit /b 0
+
+:FIND_IN_PATH
+    @set RESULT=%~dp$PATH:1
+    @endlocal && ( set %2=%RESULT%mmc )
