c32b7155c14eaff50ea7a79b6bf1377aa7638b5a
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-06-22 10:32:06 +0200

    vm_vbox_debian.cmd: suppressed output

--- homebin/vm_vbox_debian.cmd
+++ homebin/vm_vbox_debian.cmd
@@ -1,1 +1,1 @@
-start /min "Debian Wheezy 7.0 VM" VBoxManage.exe startvm "315fe370-a86f-47cb-ac09-29771c66cd09" --type headless
+@start /min "Debian Wheezy 7.5 VM" VBoxManage.exe startvm "315fe370-a86f-47cb-ac09-29771c66cd09" --type headless
