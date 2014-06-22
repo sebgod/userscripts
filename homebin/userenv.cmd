1ab6d3a9a01e2136c321d93f219d825618c911f8
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-06-22 17:48:50 +0200

    userenv.cmd: Using two lines for half-sized window

--- homebin/userenv.cmd
+++ homebin/userenv.cmd
@@ -1,6 +1,6 @@
 @if defined ProgramNative exit /b 0
 
-@prompt $E[4;32m%USERNAME%@%COMPUTERNAME%$E[0m:$E[1;35m$M$E[1;34m$P$E[0m$G
+@prompt $E[4;32m%USERNAME%@%COMPUTERNAME%$E[0m:$E[1;35m$M$E[1;34m$P$E[0m$_$G
 @set HOME=%userprofile%
 
 @set ProgramFiles32=%ProgramFiles(x86)%
