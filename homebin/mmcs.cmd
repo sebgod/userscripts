5f14066ae8e09250b1afa865617785d933f4b967
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-06-22 23:37:00 +0200

    mmcs.cmd: switching to codepage 850 inline, such that csc does not crash

--- homebin/mmcs.cmd
+++ homebin/mmcs.cmd
@@ -1,4 +1,13 @@
 @setlocal enabledelayedexpansion
+@set newCP=850
+@for /F "usebackq tokens=2 delims=:" %%A in (`chcp`) do @(
+    set oldCP=%%A
+    set oldCP=!oldCP: =!
+)
+@if %oldCP% NEQ %newCP% chcp %newCP% 1>nul
 @call %~dp0userenv
 @set keyfile="%userprofile%\Documents\GitHub\mercury\mercury.snk"
 @call mercury --use-grade-subdirs --no-detect-libgrades --sign-assembly %keyfile% -s csharp -m %*
+@set result=%ERRORLEVEL%
+@if %oldCP% NEQ %newCP% chcp %oldCP% 1>nul
+@exit /b %result%
