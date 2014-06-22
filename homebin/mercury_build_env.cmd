3ab342ffac073ead2e30ac3b6218d53ee8fb4617
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-06-22 17:49:16 +0200

    mercury_build_env.cmd: now uses the detection mechanism from mmcmake.cmd for finding the correct mmc path

--- homebin/mercury_build_env.cmd
+++ homebin/mercury_build_env.cmd
@@ -1,3 +1,28 @@
+@setlocal enabledelayedexpansion
 @call %~dp0mercury_dev_env
 @chcp 850
+
+@if defined MERCURY_HOME (
+    call :SET_HOME "%MERCURY_HOME%" MERCURY_BIN
+) else (
+    call :FIND_IN_PATH mercury.bat MERCURY_BIN
+)
+
+@if defined MERCURY_BIN goto :EXEC_SHELL
+@echo Cannot determine existing Mercury installation for bootstrapping
+@echo Env: MERCURY_HOME=%MERCURY_HOME%
+@exit /b 1
+
+:EXEC_SHELL
+@set PATH=%MERCURY_BIN%;%PATH%
+@echo Env: PATH=%PATH%
 @sh --login -i
+@exit /b 0
+
+:SET_HOME
+    @endlocal && (set %2=%~1\bin) && exit /b 0
+
+:FIND_IN_PATH
+    @set RESULT=%~dp$PATH:1
+    @endlocal && (set %2=%RESULT%) && exit /b 0
+
