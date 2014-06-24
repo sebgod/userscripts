ad91bb89e3d6895ed1adf74a5f83f85971e1148f
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-06-24 11:38:35 +0200

    mmcs.cmd: adjusted to German Windows XP

--- homebin/mmcs.cmd
+++ homebin/mmcs.cmd
@@ -3,9 +3,10 @@
 @for /F "usebackq tokens=2 delims=:" %%A in (`chcp`) do @(
     set oldCP=%%A
     set oldCP=!oldCP: =!
+    set oldCP=!oldCP:.=!
 )
 @if %oldCP% NEQ %newCP% chcp %newCP% 1>nul
-@call %~dp0userenv
+@call "%~dp0userenv"
 @set keyfile="%userprofile%\Documents\GitHub\mercury\mercury.snk"
 @call mercury --use-grade-subdirs --no-detect-libgrades --sign-assembly %keyfile% -s csharp -m %*
 @set result=%ERRORLEVEL%
