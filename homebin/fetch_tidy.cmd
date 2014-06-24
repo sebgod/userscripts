0af1ea719f3dbaf0fd340d2a6b4d3a7a89a31a2f
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-06-24 12:58:57 +0200

    fetch_tidy.cmd: fix pathes for WinXP, download tidy for HTML5 (exp.)

--- homebin/fetch_tidy.cmd
+++ homebin/fetch_tidy.cmd
@@ -1,13 +1,14 @@
 @setlocal enabledelayedexpansion
 @call "%~dp0userenv"
 
-@set tidy_url_zip=tidy.zip
-@set tidy_url=http://www.paehl.com/open_source/?download=%tidy_url_zip%
+@set tidy_url_base=http://www.paehl.com/open_source/?download=
 
+wget -N -P%TEMP% --user-agent=%user_agent% -N %tidy_url_base%tidy.zip
+wget -N -P%TEMP% --user-agent=%user_agent% -N %tidy_url_base%tidy2.7z
 
-wget -N -P%TEMP% --user-agent=%user_agent% -N %tidy_url%
-
-pushd %TEMP%
-7za e -y index.html@download=%tidy_url_zip%
-xcopy /y /i tidy.exe %~dp0api
-popd
+@pushd %TEMP%
+@7za e -y index.html@download=tidy.zip
+@7za e -y index.html@download=tidy2.7z
+@xcopy /y /i tidy.exe "%~dp0api"
+@copy /y tidy.exe "%~dp0api\tidy2.exe"
+@popd
