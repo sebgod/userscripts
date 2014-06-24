eed4cdbd988670f0333860c7b8c0c0e5958eb20f
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-06-24 12:58:33 +0200

    fetch_unicode_data.cmd: fixed pathes for WinXP

--- homebin/fetch_unicode_data.cmd
+++ homebin/fetch_unicode_data.cmd
@@ -8,13 +8,13 @@
 @set unicode_temp=%TEMP%\unidata_%unicode_version%
 
 @for %%z in (UCD Unihan) do @(
-    wget -P %unicode_temp% -N %unicode_url%%%z.zip 
-    7za x -Odata\%%z -y %unicode_temp%\%%z.zip
+    wget -P "%unicode_temp%" -N "%unicode_url%%%z.zip"
+    7za x -Odata\%%z -y "%unicode_temp%\%%z.zip"
 )
 
 @set unihan_data=%~dp0data\Unihan
-@mkdir %unihan_data%
+@mkdir "%unihan_data%"
 @for %%R in (tr38) do @(
     wget -P %unicode_temp%\%%R -N %unicode_reports%%%R/ 
-    call "%~dp0tidyutf8" -o %unihan_data%\%%R.xml %unicode_temp%\%%R\index.html
+    call "%~dp0tidyutf8" -o "%unihan_data%\%%R.xml" "%unicode_temp%\%%R\index.html"
 )
