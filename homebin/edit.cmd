a59d49a952ce11d35b85bbd8e17456f60b9914b7
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-04 01:15:50 +0200

    edit.cmd: supports EDITOR env; if using multiple files or a glob, then use the editor to open

--- homebin/edit.cmd
+++ homebin/edit.cmd
@@ -1,1 +1,14 @@
-@call "%~dp0execute" edit "%~1"
+@setlocal enableextensions enabledelayedexpansion
+@set file=%~1
+@set pattern=*
+@if not defined EDITOR set EDITOR=gvim
+
+@if "%~2" NEQ "" (
+    call %EDITOR% -p %*
+) else (
+    if "%file:%pattern%=%" EQU "%file%" (
+        call "%~dp0execute" edit "%~1"
+    ) else (
+        call %EDITOR% -p "%file%"
+    )
+)
