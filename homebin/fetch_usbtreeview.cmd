2362dbfdd60f09e8f7a09caaefd6e4213de3e1df
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-04 00:42:26 +0200

    fetch_usbtreeview.cmd: improve output

--- homebin/fetch_usbtreeview.cmd
+++ homebin/fetch_usbtreeview.cmd
@@ -4,16 +4,16 @@
 @set usbtreeview_zipfile=usbtreeview.zip
 @set usbtreeview_url=http://www.uwe-sieber.de/files/%usbtreeview_zipfile%
 
-pushd %temp%
-mkdir usbtreeview
-pushd usbtreeview
+@pushd %temp%
+@mkdir usbtreeview
+@pushd usbtreeview
 wget -N %usbtreeview_url%
 7za x -y %usbtreeview_zipfile%
 
-if /i %OS_ARCH_BITNESS% equ 32 (
+@if /i %OS_ARCH_BITNESS% EQU 32 @(
     pushd Win32
 ) else (
-    if /i %OS_ARCH_BITNESS% equ 64 (
+    if /i %OS_ARCH_BITNESS% EQU 64 (
         pushd x64
     ) else (
         echo Cannot handle bitness: %OS_ARCH_BITNESS% >&2
@@ -21,13 +21,13 @@ if /i %OS_ARCH_BITNESS% equ 32 (
     )
 )
 
-xcopy /y usbtreeview.exe %~dp0api
+@xcopy /y usbtreeview.exe %~dp0api
 
 :: exe subfolder
-popd
+@popd
 
 :: usbtreeview
-popd
+@popd
 
 :: temp
-popd
+@popd
