1b3ffcbe46a3e857d35ce214fa46e269afa10658
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-04 11:07:56 +0200

    ftplugin: fix wrong url, needs raw url

--- homecfg/vim/ftplugin/fetch.cmd
+++
@@ -1,5 +0,0 @@
-@setlocal enabledelayedexpansion
-@set url=https://github.com/gregsexton/MatchTag/blob/master/ftplugin/
-wget --no-check-certificate -N %url%/html.vim
-wget --no-check-certificate -N %url%/xml.vim
-
---
+++ homecfg/vim/ftplugin/fetch_html+xml.cmd
@@ -0,0 +1,5 @@
+@setlocal enabledelayedexpansion
+@set url=https://raw.githubusercontent.com/gregsexton/MatchTag/master/ftplugin/
+wget --no-check-certificate -N %url%html.vim
+wget --no-check-certificate -N %url%xml.vim
+
