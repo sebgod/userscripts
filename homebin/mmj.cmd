e5a68f8bf66b9941d92e2142aebda7ab66e24c21
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-05 21:11:07 +0200

    mmj.cmd: remove falsy command line calculation

--- homebin/mmj.cmd
+++ homebin/mmj.cmd
@@ -7,17 +7,5 @@
     @set mercury_compiler=!mercury_config_dir!mercury_compile
     @set mercury_config_dir=!mercury_config_dir:\bin\=\lib\mercury!
 )
-@set jcp=
-@for %%J in (%mercury_config_dir%\lib\java\*.jar) do @(
-    if [!jcp!]==[] (
-        set jcp=%%J
-    ) else (
-        set jcp=!jcp!;%%J
-    )
-)
 
-@if defined %jcp% (
-   @set jcp=--java-classpath %jcp%
-)
-
-@call "%mercury_compiler%" --use-grade-subdirs --no-detect-libgrades -s java %jcp% -m %*
+@call mercury --use-grade-subdirs --no-detect-libgrades -s java -m %*
