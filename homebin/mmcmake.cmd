783f2c9a1ff5e208eb34c9a234cb741111f3a0ba
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-06 20:25:52 +0200

    mmcmake.cmd: now sets the MERCURY_HOME path deterministically

--- homebin/mmcmake.cmd
+++ homebin/mmcmake.cmd
@@ -2,7 +2,7 @@
 @if defined MERCURY_HOME (
    call :SET_HOME MMC
 ) else (
-   call :FIND_IN_PATH mercury.bat MMC
+   call :FIND_IN_PATH mercury.bat MMC MERCRUY_HOME
 )
 
 @if defined MMC goto :MAKE
@@ -10,13 +10,17 @@
 @exit /b 1
 
 :MAKE
-@make MMC=%MMC% %*
+make MMC=%MMC% MERCURY_HOME=%MERCURY_HOME% %*
 @set MAKE_RESULT=%ERRORLEVEL%
 @exit /b %MAKE_RESULT%
 
 :SET_HOME
+    @setlocal enabledelayedexpansion
     @endlocal && ( set %1="%MERCURY_HOME%\bin\mmc" ) && exit /b 0
 
 :FIND_IN_PATH
+    @setlocal enabledelayedexpansion
     @set RESULT=%~dp$PATH:1
-    @endlocal && ( set %2=%RESULT%mmc ) && exit /b 0
+    @set STRIP=%RESULT%~~~
+    @set HOME=%STRIP:bin~~~=%
+    @endlocal && ( set %2=%RESULT%mmc ) && ( set %3=%HOME% exit /b 0
