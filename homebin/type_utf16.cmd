006f43a36500439c30f8033bcf323bf434a4d8ba
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-07 12:27:37 +0200

    type_utf8to16.cmd: renamed from type_utf16.cmd (too fuzzy), command is now using stdin

--- homebin/type_utf16.cmd
+++
@@ -1,9 +0,0 @@
-@setlocal enabledelayedexpansion
-@set inputFile=%~1
-@if not defined inputFile exit /b 1
-@if not exist "%inputFile%" exit /b 2
-@copy /y "%~dp0\empty_utf16le_bom.txt" "%inputFile%.utf16" 1>nul 2>nul
-@iconv -f utf-8 -t utf-16le <"%inputFile%" >>"%inputFile%.utf16"
-@type "%inputFile%.utf16"
-@del "%inputFile%.utf16"
-@exit /b 0
