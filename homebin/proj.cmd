66a672b2b36fc1d9e3d1c05fbd6ad857c4dbe130
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-08 16:39:54 +0200

    proj.cmd: prelim. working state (pathes correct; Makefile gets called)

---
+++ homebin/proj.cmd
@@ -0,0 +1,29 @@
+@setlocal enabledelayedexpansion
+@set tmplbase=%~dp0src\templates
+@set projtype=%~1
+@set command=%~2
+
+@if /i "%projtype%" == "" @(
+    echo Please provide a project type
+    exit /b 1
+)
+
+@if /i "%command%" == "" @(
+    echo Please provide a command, e.g.
+    echo init, update
+    exit /b 2
+)
+
+@set tmpldir=%tmplbase%\%projtype%
+@if not exist "%tmpldir%" @(
+    echo Cannot find templates for "%projtype%" in "%tmpldir%"
+    exit /b 3
+)
+
+@set mkfile=%tmpldir%\Makefile
+@if not exist "%mkfile%" @(
+    echo The cannot find makefile "%mkfile%"
+    exit /b 4
+)
+
+make -f "%mkfile%" "%command%"
