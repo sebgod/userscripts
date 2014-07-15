1c4d84100fca46254b0fb37a77f379b7e04ad3c6
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-15 15:51:40 +0200

    fetch_nirsoft.cmd: fetching all nirsoft executables

--- homebin/fetch_nirsoft.cmd
+++ homebin/fetch_nirsoft.cmd
@@ -1,23 +1,39 @@
 @setlocal enabledelayedexpansion
 @call "%~dp0userenv"
 
-:: maybe use http://launcher.nirsoft.net/download.html
-@set nirsoft_www=www.nirsoft.net
+@set nirsoft_pads=pads.zip
 @set nirsoft_x64_zip=x64tools.zip
-@set nirsoft_url=http://%nirsoft_www%/packages/%nirsoft_x64_zip%
+@set nirsoft_x64_url=http://www.nirsoft.net/packages/%nirsoft_x64_zip%
+@set nirsoft_pad_url=http://www.nirsoft.net/pad/%nirsoft_pads%
 
-pushd %~dp0
+@set target=%~dp0api\live.sysinternals.com
+@if not exist "%target%" mkdir "%target%"
 
-pushd %TEMP%
-wget -N %nirsoft_url%
-set absZipPath=%cd%\%nirsoft_x64_zip%
-popd
+@pushd "%TEMP%"
+    @call wget -N "%nirsoft_pad_url%"
+    @set absPadZip="%cd%\%nirsoft_pads%"
+    @if not exist nirsoft_pads mkdir nirsoft_pads
+    @pushd nirsoft_pads
+        @call 7za x -y %absPadZip%
+        :: iterating over the pad.xml files
+        :: assumes that the tags and their content are on the same line
+        @for %%X in (*.xml) do @(
+            @for /F "usebackq tokens=2,3 delims=^<^>" %%A in (%%X) do @(
+                if /i "%%A" == "Primary_Download_URL" call wget -N "%%B"
+            )
+        )
+        :: extract each program to the target directory
+        @for %%Z in (*.zip) do @(
+            call 7za x -o"%target%" -y "%%Z"
+            if exist "%target%\readme.txt" (
+                move "%target%\readme.txt" "%target%\%%~nZ_readme.txt"
+            )
+        )
+    @popd
 
-set target=api\%nirsoft_www%
-if not exist %target% mkdir %target%
+    @call wget -N "%nirsoft_x64_url%"
+    @if %OS_ARCH_BITNESS% EQU 64 set abs_64zip="%cd%\%nirsoft_x64_zip%"
+@popd
 
-pushd %target%
-7za x -y %absZipPath%
-popd
-
-popd
+:: if we have an x64 system, then overwrite the bitness specific programs
+@if defined abs64zip 7za x -o"%target%" -y %abs_64zip%
