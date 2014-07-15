3fc316f4d0c2771d13c18e3211c2585472b31754
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-15 21:55:18 +0200

    gnu_coreutils.cmd is now supported

--- homebin/gnu_coreutil.cmd
+++ homebin/gnu_coreutil.cmd
@@ -4,15 +4,13 @@
 @for %%P in ("%GNUWIN32_HOME%\bin") do @(
     if exist "%%~P\%~1.exe" @(
         set GNU_COMP_PATH=%%~P
-        goto :MAYBE_CALL
+        goto :CALL_GNU_COMP
     )
 )
 
-:MAYBE_CALL
-@if defined GNU_COMP_PATH @(
-    set PATH=%GNU_COMP_PATH%;%PATH%
-    call %*
-) else (
-    echo Cannot find a GNU compatible %~1 2>&1
-    exit /b 1
-)
+@echo Cannot find a GNU compatible %~1 2>&1
+@exit /b 1
+
+:CALL_GNU_COMP
+@set PATH=%GNU_COMP_PATH%;%PATH%
+@call %*
