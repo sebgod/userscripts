3c2224121bd4fab74e51b8c3d6acdabd0ea4e9db
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-28 12:34:54 +0200

    gnu_coreutil.cmd: allowing for other extensions

--- homebin/gnu_coreutil.cmd
+++ homebin/gnu_coreutil.cmd
@@ -1,16 +1,22 @@
 @setlocal enabledelayedexpansion enableextensions
 @call "%~dp0userenv"
 
+@set GNU_COMP_PATH=
+@set EXT=
 @for %%P in ("%GNUWIN32_HOME%\bin" "%systemdrive%\MinGW\msys\1.0\bin") do @(
-    if exist "%%~P\%~1.exe" @(
-        set GNU_COMP_PATH=%%~P
-        goto :CALL_GNU_COMP
+    @for %%E in (exe) do @(
+        if exist "%%~P\%~1.%%E" @(
+            set GNU_COMP_PATH=%%~P
+            set EXT=%%E
+            goto :CALL_WITH_PATH
+        )
     )
 )
 
 @echo Cannot find a GNU compatible %~1 2>&1
 @exit /b 1
 
-:CALL_GNU_COMP
+:CALL_WITH_PATH
 @set PATH=%GNU_COMP_PATH%;%PATH%
+:: This is always the last call (just like exec)
 @call %*
