0a2dfc4779307ab76a5cd720cc0a2bc5e088720b
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-12-19 18:42:01 +0800

    bashrc.d/mercury.sh: add agm=ag --mercury and gosr (already in macros.txt)

--- homecfg/bashrc.d/mercury.sh
+++ homecfg/bashrc.d/mercury.sh
@@ -4,8 +4,10 @@ SEGO_GITHUB=$HOME/github/sebgod
 MERCURY_FORK=$SEGO_GITHUB/mercury
 MERCURY_MAIN=$HOME/github/mercury/mercury-dev
 alias msego="cd ${MERCURY_FORK}"
-alias mdev="cd $MERCURY_MAIN"
+alias mdev="cd ${MERCURY_MAIN}"
 alias ucd="cd ${SEGO_GITHUB}/mercury-unicode"
+alias gosr="cd ${SEGO_GITHUB}/shift-reduce"
 alias m="mmc --use-grade-subdirs --make"
 alias sm="sudo mmc --use-grade-subdirs --make"
 alias mmc-boot="sudo docker run -v=$${PWD}:/data sebgod/mercury-bootstrap:latest --use-grade-subdirs"
+alias agm="ag --mercury"
