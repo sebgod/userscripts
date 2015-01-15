00e9eb5291d90e37b95ef4d125b1984d614149f5
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-01-15 22:50:17 +0800

    change_display_resolution.cmd: silent

--- homebin/change_display_resolution.cmd
+++ homebin/change_display_resolution.cmd
@@ -1,6 +1,6 @@
 @setlocal enabledelayedexpansion
 @call userenv.cmd
-set out="%~dp0api\%~n0.exe"
-set src="%~n0.cs"
-call cscu /target:winexe /out:%out% %src%
-call %out% 1600 900
+@set out="%~dp0api\%~n0.exe"
+@set src="%~n0.cs"
+@call cscu /target:winexe /out:%out% %src%
+@call %out% 1600 900
