593c45dd88462ca709431cf0c9d68277812a4aaa
Sebastian Godelet <sebastian.godelet@outlook>
2015-01-18 12:31:38 +0800

    bashrc.d/mercury.sh: remove /usr/local
    Install Mercury into $HOME from now on

--- homecfg/bashrc.d/mercury.sh
+++ homecfg/bashrc.d/mercury.sh
@@ -1,10 +1,6 @@
-export MERCURY_HOME=/usr/local/mercury-DEV
-[ -d $MERCURY_HOME/bin ] && export PATH=$MERCURY_HOME/bin:$PATH
 SEGO_GITHUB=$HOME/github/sebgod
 MERCURY_FORK=$SEGO_GITHUB/mercury
-MERCURY_MAIN=$HOME/github/mercury/mercury-dev
-alias msego="cd ${MERCURY_FORK}"
-alias mdev="cd ${MERCURY_MAIN}"
+alias mdev="cd ${MERCURY_FORK}"
 alias ucd="cd ${SEGO_GITHUB}/mercury-unicode"
 alias gosr="cd ${SEGO_GITHUB}/shift-reduce"
 alias m="mmc --use-grade-subdirs --make"
