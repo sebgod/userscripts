bf42570f34dfb9fee3c575a7e9ce4ff126a66665
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-03-11 09:57:46 +0800

    bashrc.d/java.sh: export JDK+JRE home

--- homecfg/bashrc.d/java.sh
+++ homecfg/bashrc.d/java.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-export CLASSPATH=/usr/share/java/\*:$CLASSPATH
-JAVA_HOME=`readlink -f /usr/bin/java  | sed "s:bin/java::"`
-[ -r "$JAVA_HOME" ] || JAVA_HOME=`readlink -f /usr/bin/javac | sed "s:bin/javac::"`
+if [ -z "$CLASSPATH" ] ; then
+    export CLASSPATH=/usr/share/java/\*
+fi
+
+JRE_HOME=`readlink -f /usr/bin/java   | sed "s:/bin/java::"`
+JAVA_HOME=`readlink -f /usr/bin/javac  | sed "s:/bin/javac::"`
+
+if [ -r "$JAVA_HOME" ] ; then
+    export JDK_HOME=$JAVA_HOME
+else
+    JAVA_HOME=$JRE_HOME
+fi
+
 export JAVA_HOME
+export JRE_HOME
