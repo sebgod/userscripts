141224138e66582ed815cc2a358032a8e69d2a29
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-03-31 08:33:10 +0800

    delete jssc (now in its own repository)

--- homebin/jssc.cmd
+++
@@ -1,4 +0,0 @@
-@setlocal enabledelayedexpansion enableextensions
-@set ID=%RANDOM%_%RANDOM%
-@del /q /f "%TEMP%\_jssc_*.exe" 1>nul 2>&1
-@jsc /codepage:65001 /nologo /fast+ /out:"%TEMP%\_jssc_%ID%.exe" "%~dpn0.jss" && "%TEMP%\_jssc_%ID%.exe" %*
