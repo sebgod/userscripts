e130e3b6d8d7e5ff5b3aa5cc79906040376066b7
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-05-08 00:31:39 +0800

    fix working dir binding

--- homebin/mmc-docker.cmd
+++ homebin/mmc-docker.cmd
@@ -2,4 +2,4 @@
 @for /F "usebackq" %%P in (`pwd`) do @set __PWD=%%P
 @set __TMP=%TEMP:\=/%
 @set __TMP=/%__TMP::=%
-@call docker run -it --read-only=true -v %__TMP%:/tmp -v %__PWD%:/var/tmp:rw sebgod/mercury-stable:latest --no-use-symlinks %*
+@call docker run -it --read-only=true -v %__TMP%:/tmp -v %__PWD%:/var/tmp/mercury:rw sebgod/mercury-stable:latest --no-use-symlinks %*
