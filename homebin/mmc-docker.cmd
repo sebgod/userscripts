881efc0f7988a2b4cbe903c65382dab41467dd5d
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-05-07 17:04:53 +0800

    introduce mmc-docker command

---
+++ homebin/mmc-docker.cmd
@@ -0,0 +1,5 @@
+@setlocal enabledelayedexpansion enableextensions
+@for /F "usebackq" %%P in (`pwd`) do @set __PWD=%%P
+@for /F "usebackq" %%U in (`id -u %USER%`) do @set __UID=%%U
+
+call docker run -u %__UID% -it -v %__PWD%:/tmp/mercury --read-only=true sebgod/mercury-stable:latest %*
