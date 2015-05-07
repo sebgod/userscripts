3b3ca6fa7fe6594df5bcd3498d43942e479f790e
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-05-07 17:03:52 +0800

    update msysgit version

--- homebin/fetch_git.cmd
+++ homebin/fetch_git.cmd
@@ -2,10 +2,10 @@
 @call "%~dp0userenv"
 
 @set git_basename=PortableGit
-@set git_version=1.9.5-preview20141217
+@set git_version=1.9.5-preview20150319
 @set git_7zipfile=%git_basename%-%git_version%.7z
 @set git_url=https://github.com/msysgit/msysgit/releases/download/Git-%git_version%/%git_7zipfile%
-curl -L -o %TEMP%\%git_7zipfile% "%git_url%"
+curl -k -L -o %TEMP%\%git_7zipfile% "%git_url%"
 
 pushd %~dp0api
 7za x -y -ogit "%TEMP%\%git_7zipfile%"
