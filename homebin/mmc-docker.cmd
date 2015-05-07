74a75c824af3dcc39c47a7fb75f5baf9605c28ab
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-05-07 23:10:35 +0800

    using /var/tmp for mmc-docker

--- homebin/mmc-docker.cmd
+++ homebin/mmc-docker.cmd
@@ -1,6 +1,5 @@
 @setlocal enabledelayedexpansion enableextensions
-@call userenv
 @for /F "usebackq" %%P in (`pwd`) do @set __PWD=%%P
-@for /F "usebackq" %%U in (`id -u %USER%`) do @set __UID=%%U
-
-@call docker run -u %__UID% -it -v %__PWD%:/tmp/mercury --read-only=true sebgod/mercury-stable:latest %*
+@set __TMP=%TEMP:\=/%
+@set __TMP=/%__TMP::=%
+@call docker run -it --read-only=true -v %__TMP%:/tmp -v %__PWD%:/var/tmp:rw sebgod/mercury-stable:latest --no-use-symlinks %*
