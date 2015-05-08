43ca8515ea65c26e53733897a20f9edd3f13cc8e
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-05-08 14:00:02 +0800

    moved mmc-docker to docker-mercury-stable*

--- homebin/mmc-docker.cmd
+++
@@ -1,5 +0,0 @@
-@setlocal enabledelayedexpansion enableextensions
-@for /F "usebackq" %%P in (`pwd`) do @set __PWD=%%P
-@set __TMP=%TEMP:\=/%
-@set __TMP=/%__TMP::=%
-@call docker run -it --read-only=true -v %__TMP%:/tmp -v %__PWD%:/var/tmp/mercury:rw sebgod/mercury-stable:latest --no-use-symlinks %*
