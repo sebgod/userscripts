1068d78905b76c9956a9dfdb5d52198c348513cb
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-05-12 16:53:58 +0800

    bin/mercury_build_linux.sh: enable building of compiler in temporary directory

--- homecfg/bin/mercury_build_linux.sh
+++ homecfg/bin/mercury_build_linux.sh
@@ -3,7 +3,8 @@
 
 SOURCE=${MERCURY_GIT-~/github/sebgod/mercury}
 CC=${MERCURY_CC-gcc}
-TARGET_WITH_CC=$HOME/mercury/dev-${CC//[[:blank:]]/}
+CC_NO_BLANKS=${CC//[[:blank:]]/}
+TARGET_WITH_CC=$HOME/mercury/dev-${CC_NO_BLANKS}
 CONFIGURE=configure
 CONFIGURE_EXTRA=
 LAST_COMMAND=true
@@ -14,10 +15,10 @@ case $CC in
     mingw*)
         TARGET=$TARGET_WITH_CC
         CONFIGURE=tools/configure_mingw_cross
-        LAST_COMMAND=ln -s $HOME/bin/mercury_compile $TARGET/bin
+        LAST_COMMAND="ln -f -s $HOME/bin/mercury_compile $TARGET/bin"
         case $CC in
             mingw32)
-                CONFIGURE_EXTRA="--host=i686-pc-mingw32"
+                CONFIGURE_EXTRA="--host=i686-w64-mingw32"
                 ;;
             mingw64)
                 CONFIGURE_EXTRA="--host=x86_64-w64-mingw32"
@@ -25,7 +26,7 @@ case $CC in
             *)
                 echo "WARN: $CC is not supported!"
                 ;;
-        )
+        esac
         ;;
     *)
         TARGET=$TARGET_WITH_CC
@@ -35,7 +36,20 @@ esac
 PARALLEL=-j4
 LIBGRADES=${MERCURY_LIBGRADES-asm_fast.gc,java,csharp,erlang}
 
-pushd $SOURCE
+echo -ne "\033]0;build mmc cc:${CC} grades:${LIBGRADES}\007"
+
+TMP_BUILD_DIR=/tmp/mercury-${CC_NO_BLANKS}
+mkdir -p $TMP_BUILD_DIR
+
+pushd "$SOURCE"
+
+git checkout build &&
+git rebase master &&
+
+rsync -r . $TMP_BUILD_DIR
+
+pushd $TMP_BUILD_DIR
+
 git checkout build &&
 git rebase master &&
 aclocal -I m4 &&
@@ -46,13 +60,11 @@ autoconf &&
     --enable-new-mercuryfile-struct &&
 touch Mmake.params &&
 touch Mercury.options &&
-mmake depend &&
-mmake MMAKEFLAGS=$PARALLEL &&
-# this does not work on Fedora currently
-#tools/bootcheck $PARALLEL &&
-#cd stage2 &&
-mmake install MMAKEFLAGS=$PARALLEL &&
-mmake realclean &&
+make depend &&
+make PARALLEL=$PARALLEL &&
+make install PARALLEL=$PARALLEL &&
 $LAST_COMMAND
 
-popd
+popd # $SOURCE
+popd # $TMP_BUILD_DIR
+rm -fR $TMP_BUILD_DIR
