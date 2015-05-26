b5bbb2c09c3704f46f4c063c3f9628e2b9fbb5c8
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-05-26 15:30:49 +0800

    b2d: can now be used for all boot2docker commands

--- homebin/b2d.cmd
+++ homebin/b2d.cmd
@@ -1,11 +1,34 @@
-@call boot2docker init
-@for /F "usebackq tokens=1,2,* delims== " %%A in (`boot2docker start 2^>^&1`) do @(
-    if "%%~A" EQU "set" set %%B=%%C
-)
-@if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+:: Supported commands:
+::   init                Create a new Boot2Docker VM.
+::   up|start|boot       Start VM from any states.
+::   ssh [ssh-command]   Login to VM via SSH.
+::   save|suspend        Suspend VM and save state to disk.
+::   down|stop|halt      Gracefully shutdown the VM.
+::   restart             Gracefully reboot the VM.
+::   poweroff            Forcefully power off the VM (may corrupt disk image).
+::   reset               Forcefully power cycle the VM (may corrupt disk image).
+::   delete|destroy      Delete Boot2Docker VM and its disk image.
+::   config|cfg          Show selected profile file settings.
+::   info                Display detailed information of VM.
+::   ip                  Display the IP address of the VM's Host-only network.
+::   shellinit           Display the shell commands to set up the Docker client.
+::   status              Display current state of VM.
+::   download            Download Boot2Docker ISO image.
+::   upgrade             Upgrade the Boot2Docker ISO image (restart if running).
+::   version             Display version information.
 
-@setlocal enabledelayedexpansion enableextensions
-@for %%V in (HOST CERT_PATH TLS_VERIFY) do @(
-    @if not defined DOCKER_%%V exit /b 1
-    @echo DOCKER_%%V=!DOCKER_%%V!
+@echo %1 | findstr "start up boot shellinit" >nul && (
+    call boot2docker init
+    for /F "usebackq tokens=1,2,* delims== " %%A in (`boot2docker start 2^>^&1`) do @(
+        if "%%~A" EQU "set" set %%B=%%C
+    )
+    if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+    setlocal enabledelayedexpansion enableextensions
+    for %%V in (HOST CERT_PATH TLS_VERIFY) do @(
+        if not defined DOCKER_%%V exit /b 1
+        echo DOCKER_%%V=!DOCKER_%%V!
+    )
+) || (
+    call boot2docker %*
 )
